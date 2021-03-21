572a8467fe2eea6beab062cf736da4a13ad2b1a8
Adi Roiban <adi.roiban@chevah.com>
2021-03-21 02:01:21 +0000

    use 2 cpus.

--- README.rst
+++ README.rst
@@ -9,3 +9,19 @@ Needs latest OpenSSH::
 
     ssh-keygen -M generate -O bits=2048 moduli-2048.candidates
     ssh-keygen -M screen -f moduli-2048.candidates moduli-2048
+
+Limits for GitHub Hosted VM [1]:
+* 6 hours per job
+* 72 hours per workflow
+* 20 concurrent jobs
+
+Generating the candidates is fast. About 15 minutes for 8k.
+
+Validating is slow:
+* 4096 - 1 hour
+* 6144 - 10 hours
+* 7680 - 30 hours
+* 8192
+
+
+[1] https://docs.github.com/en/actions/reference/usage-limits-billing-and-administration#usage-limits
--- chevah_format.py
+++ chevah_format.py
@@ -1,52 +1,48 @@
 """
-Combine the OpenSSH moduli and output in Chevah Python format.
-
-The files should end with the file size.
+Convert from OpenSSH moduli to Chevah Python format.
 """
 import os
 import sys
 
 
-def generate_ssh_moduli(source_start, output_path):
+def generate_ssh_moduli(input_path, output_path):
     """
     Generate the Python source file which stores the SSH moduli values.
     """
 
     # Lines for the Python module where to store the SSH moduli values.
     python_moduli = [
-        '"""'
-        'SSH Moduli files for this release.'
-        ''
-        'Generate the content of file using'
-        '`$ paver test_remote gk-ssh-moduli`'
-        'and then copy the output of the step below.'
+        '"""',
+        'SSH Moduli files for this release.',
+        '',
+        'Generate from GHA.',
         '"""',
         '',
-        'STATIC_MODULI = {}'
+        'STATIC_MODULI = {}',
+        '',
         ]
 
-    source_files = [n for n in os.listdir() if n.startswith(source_start)]
+    initialized_sizes = []
 
-    for validated_file in sorted(source_files):
+    with open(input_path, 'r') as stream:
+        for line in stream.readlines():
+            line = line.strip()
+            if not line or line[0] == '#':
+                continue
 
-        size = int(validated_file[len(source_start):])
+            tim, typ, tst, tri, bits, gen, mod = line.split()
 
-        python_moduli.append('')
-        python_moduli.append('STATIC_MODULI[%s] = []' % (size,))
+            size = int(bits) + 1
 
-        with open(validated_file, 'rb') as stream:
-            for line in stream.readlines():
-                line = line.strip()
-                if not line or line[0] == '#':
-                    continue
-                tim, typ, tst, tri, bits, gen, mod = line.split()
-                if size != int(bits) + 1:
-                    raise Exception('Mismatch in moduli size.')
+            if size not in initialized_sizes:
+                initialized_sizes.append(size)
+                python_moduli.append('')
+                python_moduli.append('STATIC_MODULI[%s] = []' % (size,))
 
-                gen = int(gen)
-                mod = int(mod, 16)
-                python_moduli.append(
-                    'STATIC_MODULI[%s].append((%s, %s))' % (size, gen, mod))
+            gen = int(gen)
+            mod = int(mod, 16)
+            python_moduli.append(
+                'STATIC_MODULI[%s].append((%s, %s))' % (size, gen, mod))
 
     # Have empty line at the end.
     python_moduli.append('')
@@ -57,7 +53,7 @@ def generate_ssh_moduli(source_start, output_path):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print('Usage: SOURCE_START_NAME OUTPUT_PATH')
+        print('Usage: INPUT_PATH OUTPUT_PATH')
         sys.exit(1)
 
-    generate_ssh_moduli(sys.argv[1], sys.argv[1])
+    generate_ssh_moduli(sys.argv[1], sys.argv[2])
